1a0c46dab21f1b5b7332daa1cd569b6f3c3e664e
morilisten <yangshun1997@gmail.com>
2025-07-25 09:18:20 +0800

    docs: theheader update

--- src/components/Theheader.vue
+++ src/components/Theheader.vue
@@ -9,10 +9,10 @@ import Basebutton from '@/components/Basebutton.vue'
             <img :src="logo" alt="LOGO" class="flex items-center w-[70px] h-[70px] p-[2px]" />
         <!-- 導覽連結 -->
         <nav class="flex items-center space-x-6 text-white text-sm">
-            <a href="#" class="hover:text-orange-400 transition">貼文區</a>
-            <a href="#" class="hover:text-orange-400 transition">武器展示區</a>
-            <a href="#" class="hover:text-orange-400 transition">活動專區</a>
-            <a href="#" class="hover:text-orange-400 transition">關於我們</a>
+            <a href="#" class="h5 hover:text-orange-400 transition">貼文區</a>
+            <a href="#" class="h5 hover:text-orange-400 transition">武器展示區</a>
+            <a href="#" class="h5 hover:text-orange-400 transition">活動專區</a>
+            <a href="#" class="h5 hover:text-orange-400 transition">關於我們</a>
         </nav>
     </header>
-</template>
+</template> 
