8f6ee6496e0ca04b60c1f3c2cf9e8ea89f09fcc0
morilisten <yangshun1997@gmail.com>
2025-08-05 14:20:31 +0800

    fixed: getPublicImg 範例

--- src/utils/getPublicImg.js
+++ src/utils/getPublicImg.js
@@ -6,5 +6,6 @@ export const getPublicImg = (filename) => {
 // <script setup>
 // import { getPublicImg } from '@/utils/getPublicImg'
 
-// const 變數 = getPublicAsset('filename')
+// const window_img_d = getPublicAsset('filename')
+// const window_img_n = getPublicAsset('Home_img/Window_n.png')
 // </script>
